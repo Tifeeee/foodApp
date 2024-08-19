d49abd10cc8ce52c2c35e88a234a22eab0b4e15c
Boluwatife Olaoye <Olaoyeboluwatife20@gmail.com>
2024-08-19 16:40:42 +0100

    wip on the error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Nav from './Components/Navigation/Nav'
 import Hero from './Components/HeroSection/Hero'
 import Products from './Components/Products/Products'
 import About from './Components/About/About'
 import Team from './Components/Team/Team'
 import Footer from './Components/Footer/Footer'
+import Nav from './Components/Navigation/Nav'
 
 
 function App() {
--- src/Components/Navigation/nav.jsx
+++ src/Components/Navigation/nav.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const nav = () => {
+const Nav = () => {
   return (
     <>
-  <nav className= "navBar">
-    <div >      
-    <h1 className='brandName' >VeggieBite.</h1>
-    </div>
-    <ul className= "info">
-    <li>Veggie Guide</li>
-    <li>Blog</li>
-    <li>FAQ</li>
-    <li>Lifestyle</li>
-    <li>Community</li>
-    </ul>
-    </nav>
+      <nav className="navBar">
+        <div>
+          <h1 className="brandName">VeggieBite.</h1>
+        </div>
+        <ul className="info">
+          <li>Veggie Guide</li>
+          <li>Blog</li>
+          <li>FAQ</li>
+          <li>Lifestyle</li>
+          <li>Community</li>
+        </ul>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default nav
+export default Nav;
